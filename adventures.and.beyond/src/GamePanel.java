8829a010b47a482b10700868b7702c6be31072df
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-18 23:01:30 -0800

    feat:Binding keyhandler class to KeyListner event.

--- adventures.and.beyond/src/GamePanel.java
+++ adventures.and.beyond/src/GamePanel.java
@@ -15,11 +15,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int getMaxScreenHeight = maxScreenRow * tileSize; //576 pixels
     // Game will be run on this thread
     Thread gameThread;
+    final KeyHandler keyHandler;
+
+    //FPS
+    int fps = 60;
+
+    // Set Player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(maxScreenWidth, getMaxScreenHeight));// defining panel size
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true); // improve game's rendering performance
+        this.keyHandler = new KeyHandler();
+        this.addKeyListener(this.keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -27,6 +40,7 @@ public class GamePanel extends JPanel implements Runnable {
         this.gameThread.start();
     }
 
+     */
     @Override
     public void run() {
         while (this.gameThread != null) {
@@ -46,7 +60,15 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-
+        if (keyHandler.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyHandler.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyHandler.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyHandler.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
     @Override
@@ -55,7 +77,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D graphics2D = (Graphics2D) graphic;
         graphics2D.setColor(Color.WHITE);
-        graphics2D.fillRect(100, 100, tileSize, tileSize);
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
         //Disposes of this graphics context and releases any system resources that it is using.
         graphics2D.dispose();
     }
