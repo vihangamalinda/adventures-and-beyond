524dfd908dc97868772bcd9b51913364108c7de9
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-20 02:23:56 -0800

    feat:Giving a sense of direction and a state (idle/moving) to an entity.

--- adventures.and.beyond/src/entity/Entity.java
+++ adventures.and.beyond/src/entity/Entity.java
@@ -1,18 +1,40 @@
 package entity;
 
+import directionEnum.Direction;
+
 public class Entity {
     private int positionX;
     private int positionY;
     private int speed;
+    private Direction direction;
+    private boolean isIdle;
 
-    public Entity(int positionX, int positionY, int speed) {
+    public Entity(int positionX, int positionY, int speed,Direction direction,boolean isIdle) {
         this.positionX = positionX;
         this.positionY = positionY;
         this.speed = speed;
+        this.direction=direction;
+        this.isIdle=isIdle;
     }
 
     public int getPositionX() {
         return positionX;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public boolean isIdle() {
+        return isIdle;
+    }
+
+    public void setIdle(boolean idle) {
+        isIdle = idle;
     }
 
     public void setPositionX(int positionX) {
