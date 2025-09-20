51baad410bb5e19eb93ac164bda8c3181347e54c
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-19 19:56:13 -0800

    feat: Adding entity and player class.

---
+++ adventures.and.beyond/src/entity/Entity.java
@@ -0,0 +1,37 @@
+package entity;
+
+public class Entity {
+    private int positionX;
+    private int positionY;
+    private int speed;
+
+    public Entity(int positionX, int positionY, int speed) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.speed = speed;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
---
+++ adventures.and.beyond/src/entity/Player.java
@@ -0,0 +1,71 @@
+package entity;
+
+import helper.Helper;
+import main.GamePanel;
+import main.KeyHandler;
+
+import java.awt.*;
+
+public class Player extends Entity {
+    private GamePanel gamePanel;
+    private KeyHandler keyHandler;
+
+    public Player(int positionX, int positionY, int speed, GamePanel gamePanel, KeyHandler keyHandler) {
+        super(positionX, positionY, speed);
+        this.gamePanel = gamePanel;
+        this.keyHandler = keyHandler;
+    }
+
+    public void update() {
+        if (keyHandler.upPressed) {
+            this.moveUpDirection();
+        } else if (keyHandler.downPressed) {
+            this.moveDownDirection();
+        } else if (keyHandler.leftPressed) {
+            this.moveLeftDirection();
+        } else if (keyHandler.rightPressed) {
+            this.moveRightDirection();
+        }
+    }
+
+    public void paintComponent(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(this.getPositionX(), this.getPositionY(), Helper.TILE_SIZE, Helper.TILE_SIZE);
+    }
+
+    private void moveUpDirection() {
+        int newPositionY = this.getPositionY() - this.getSpeed();
+        this.setPositionY(newPositionY);
+    }
+
+    private void moveDownDirection() {
+        int newPositionY = this.getPositionY() + this.getSpeed();
+        this.setPositionY(newPositionY);
+    }
+
+    private void moveLeftDirection() {
+        int newPositionX = this.getPositionX() - this.getSpeed();
+        this.setPositionX(newPositionX);
+    }
+
+    private void moveRightDirection() {
+        int newPositionX = this.getPositionX() + this.getSpeed();
+        this.setPositionX(newPositionX);
+    }
+
+    public GamePanel getGamePanel() {
+        return gamePanel;
+    }
+
+    public void setGamePanel(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
+    public KeyHandler getKeyHandler() {
+        return keyHandler;
+    }
+
+    public void setKeyHandler(KeyHandler keyHandler) {
+        this.keyHandler = keyHandler;
+    }
+}
