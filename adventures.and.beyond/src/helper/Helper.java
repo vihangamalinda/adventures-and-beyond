8e529b054935ba6c7d51dbfd72fb3f87cd91c4b9
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-20 21:51:23 -0800

    fix:Changing the scope of  constants.

--- adventures.and.beyond/src/helper/Helper.java
+++ adventures.and.beyond/src/helper/Helper.java
@@ -14,8 +14,8 @@ public class Helper {
     public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * UP_SCALE;// 48*48 tile
 
     // maintaining 4:3 size ratio on width to height ratio
-    final static int MAX_SCREEN_COLUMN = 16;
-    final static int MAX_SCREEN_ROW = 12;
+    public final static int MAX_SCREEN_COLUMN = 16;
+    public final static int MAX_SCREEN_ROW = 12;
 
     public static final int WINDOW_MAX_SCREEN_WIDTH = MAX_SCREEN_COLUMN * TILE_SIZE;//768 pixels
     public static final int WINDOW_MAX_SCREEN_HEIGHT = MAX_SCREEN_ROW * TILE_SIZE;//576 pixels
