18aa06e3f2a958684de53db86cb4512d2451a908
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-21 19:04:39 -0800

    feat: Tile drawing logic should always depend on users position.

--- adventures.and.beyond/src/tile/TileManager.java
+++ adventures.and.beyond/src/tile/TileManager.java
@@ -1,12 +1,14 @@
 package tile;
 
+import entity.Player;
+import helper.Constant;
 import helper.Loader;
 import main.GamePanel;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import static helper.Constant.TILE_SIZE;
+import static helper.Constant.*;
 
 
 public class TileManager {
@@ -74,15 +76,47 @@ public class TileManager {
 //        graphics2D.drawImage(getTileByIndex(1).getBufferedImage(),48,0, Helper.TILE_SIZE,Helper.TILE_SIZE,null);
 //        graphics2D.drawImage(getTileByIndex(2).getBufferedImage(),96,0, Helper.TILE_SIZE,Helper.TILE_SIZE,null);
 
+        Player player=gamePanel.getPlayer();
+//        int drawCol =(player.getWorldPositionX()/TILE_SIZE)- (MAX_SCREEN_COLUMN/2);
+//        int drawRow =(player.getWorldPositionY()/TILE_SIZE)- (MAX_SCREEN_COLUMN/2);
 
-        for (int row=0;row<this.mapTileMatrix.length;row++){
-            int[] columValues =this.mapTileMatrix[row];
-            for (int col=0;col<columValues.length;col++){
-                int positionX =col* Constant.TILE_SIZE;
-                int positionY =row* Constant.TILE_SIZE;
-                BufferedImage image =getTileByIndex(columValues[col]).getBufferedImage();
-                graphics2D.drawImage(image,positionX,positionY, Constant.TILE_SIZE, Constant.TILE_SIZE,null);
+        int drawMapCol =(player.getPlayerAbsoluteCenterX()/TILE_SIZE)- (MAX_SCREEN_COLUMN/2);
+        int drawMapRow =(player.getPlayerAbsoluteCenterY()/TILE_SIZE)- (MAX_SCREEN_COLUMN/2);
+
+
+
+        if(drawMapRow<0){
+            drawMapRow=0;
+        }else if((drawMapRow+MAX_SCREEN_ROW)>MAX_WORLD_ROWS){
+            drawMapRow =MAX_WORLD_ROWS-MAX_SCREEN_ROW;
+        }
+
+        if(drawMapCol<0){
+            drawMapCol=0;
+        }else if(drawMapCol+MAX_SCREEN_COLUMN>MAX_WORLD_COLUMNS){
+            drawMapCol =MAX_WORLD_COLUMNS-MAX_SCREEN_COLUMN;
+        }
+
+
+
+
+//        System.out.println("Player worldMapPositions, (X,Y): ("+player.getWorldPositionX()+":"+player.getWorldPositionY()+")");
+//        System.out.println("Player worldMapPositions, (currentRow,currentColumn): ("+(player.getWorldPositionY()/TILE_SIZE)+":"+(player.getWorldPositionX()/TILE_SIZE)+")");
+//        System.out.println("Player Draw, (drawRow,drawCol): ("+drawMapRow+":"+drawMapCol+")");
+
+
+        for (int windowRow = 0; windowRow< Constant.MAX_SCREEN_ROW; windowRow++){
+            int[] columValues =this.mapTileMatrix[drawMapRow];
+            int currentMapColumn=   drawMapCol;
+            for (int windowCol = 0; windowCol< MAX_SCREEN_COLUMN; windowCol++){
+                int windowPositionX =windowCol* TILE_SIZE;
+                int windowPositionY =windowRow* TILE_SIZE;
+                BufferedImage image =getTileByIndex(columValues[currentMapColumn]).getBufferedImage();
+
+                graphics2D.drawImage(image,windowPositionX,windowPositionY, TILE_SIZE, TILE_SIZE,null);
+                currentMapColumn++;
             }
+            drawMapRow++;
         }
 
     }
