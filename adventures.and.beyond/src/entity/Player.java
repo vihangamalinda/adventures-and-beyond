61506eec36128085638e33b74757c3e690657880
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-22 21:35:25 -0800

    feat: Adding solidArea inorder to measure collision detection.

--- adventures.and.beyond/src/entity/Player.java
+++ adventures.and.beyond/src/entity/Player.java
@@ -8,7 +8,7 @@ import main.KeyHandler;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import static helper.Constant.PLAYER_UP_SCALE;
+import static helper.Constant.*;
 import static helper.PlayerSpriteManager.getPlayerImageByIndex;
 
 public class Player extends Entity {
@@ -40,7 +40,7 @@ public class Player extends Entity {
 
 
     public Player(int positionX, int positionY, GamePanel gamePanel, KeyHandler keyHandler) {
-        super(positionX, positionY, movementSpeed, Direction.FACING_FORWARD, true);
+        super(positionX, positionY, movementSpeed, Direction.FACING_FORWARD, true,new Rectangle(PLAYER_SOLID_AREA_START_X,PLAYER_SOLID_AREA_START_Y,PLAYER_SOLID_AREA_WIDTH,PLAYER_SOLID_AREA_HEIGHT),false);
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
         this.initializeCentralizeCamera();
@@ -90,10 +90,18 @@ public class Player extends Entity {
           graphics2D.setColor(Color.WHITE);
           graphics2D.fillRect( this.screenPositionX,  this.screenPositionY, scaledPlayer, scaledPlayer);
 
+        drawSolidArea(graphics2D);
+
 //        graphics2D.setColor(Color.WHITE);
 //        graphics2D.fillRect(this.getPositionX() +200, this.getPositionY() +300, Helper.TILE_SIZE, Helper.TILE_SIZE);
 //        BufferedImage blueImg =Helper.blueImg;
 //        graphics2D.drawImage(blueImg,this.getPositionX() +200,this.getPositionY() +300,Helper.TILE_SIZE,Helper.TILE_SIZE,null);
+    }
+
+    private void drawSolidArea(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.RED);
+        Rectangle rectangle = this.getSolidArea();
+        graphics2D.fillRect( this.screenPositionX +rectangle.x,  this.screenPositionY+rectangle.y, rectangle.width, rectangle.height);
     }
 
     private void initializeCentralizeCamera() {
--- adventures.and.beyond/src/helper/Constant.java
+++ adventures.and.beyond/src/helper/Constant.java
@@ -25,4 +25,10 @@ public class Constant {
     public final static int WORLD_MAP_WIDTH = TILE_SIZE * MAX_WORLD_COLUMNS;
     public final static int WORLD_MAP_HEIGHT = TILE_SIZE * MAX_WORLD_ROWS;
 
+    //Player Solid Area
+    public final static int PLAYER_SOLID_AREA_START_X=8 *PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_START_Y=16 *PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_WIDTH=32 *PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_HEIGHT=32 *PLAYER_UP_SCALE;
+
 }
