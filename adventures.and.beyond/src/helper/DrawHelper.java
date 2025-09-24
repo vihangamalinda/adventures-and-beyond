b848e5d61dcb0ff10a9accc10e1d9071b2552fa3
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-24 05:43:39 -0800

    feat: Adding helper method to determine objects location on window screen.

--- adventures.and.beyond/src/helper/DrawHelper.java
+++ adventures.and.beyond/src/helper/DrawHelper.java
@@ -38,4 +38,34 @@ public class DrawHelper {
     private static int getCurrentWindowStartColum(Player player) {
         return player.getCurrentColOnWorldMap() - MAX_SCREEN_COLUMN / 2;
     }
+
+    public static int getObjWindowPositionYRespectiveToPlayer(int positionY,Player player){
+        int objPlayerMapDifferance = player.getCurrentRowOnWorldMap()-(positionY/TILE_SIZE);
+        int playerScreenCol = player.getScreenPositionY()/TILE_SIZE;
+
+        int value;
+        if(objPlayerMapDifferance<0){
+            // Object is below Player position on Map
+            value= playerScreenCol + Math.abs(objPlayerMapDifferance);
+        }else{
+            // Object is above Player position on Map
+            value =playerScreenCol+ (objPlayerMapDifferance*(-1));
+        }
+        return value*TILE_SIZE;
+    }
+    public static int getObjWindowPositionXRespectiveToPlayer(int positionX,Player player){
+        int objPlayerMapDifferance = player.getCurrentColOnWorldMap()-(positionX/TILE_SIZE);
+//        System.out.println("objPlayerMapDifferance"+objPlayerMapDifferance);
+        int playerScreenRow= player.getScreenPositionX()/TILE_SIZE;
+
+        int value;
+        if(objPlayerMapDifferance<0){
+            // Object is leftward to Player position on Map
+            value= playerScreenRow + Math.abs(objPlayerMapDifferance);
+        }else{
+            // Object is rightward ti Player position on Map
+            value =playerScreenRow+ (objPlayerMapDifferance*(-1));
+        }
+        return value*TILE_SIZE;
+    }
 }
