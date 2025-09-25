83add5958b00188caac8d1b1435ac5ad42d0840d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-25 08:22:59 -0800

    feat: Adding key object to open treasure box.

--- adventures.and.beyond/src/object/InteractableObjectManager.java
+++ adventures.and.beyond/src/object/InteractableObjectManager.java
@@ -19,9 +19,11 @@ public class InteractableObjectManager {
     private InteractableObject[] initializeInteractableObjects() {
         InteractableObject key = new KeyObject(false, 28 * TILE_SIZE, 10 * TILE_SIZE, "KEY_07", true);
         InteractableObject door = new DoorObject(false, 25 * TILE_SIZE, 9 * TILE_SIZE, "DOOR_5", "KEY_07", true);
-        InteractableObject treasure = new TreasureObject(false, 21 * TILE_SIZE, 8 * TILE_SIZE, "TREASURE_5", "KEY_08", true);
 
-        return new InteractableObject[]{key, door, treasure};
+        InteractableObject treasure = new TreasureObject(false, 21 * TILE_SIZE, 8 * TILE_SIZE, "TREASURE_05", "KEY_08", true);
+        InteractableObject key2 = new KeyObject(false, 16 * TILE_SIZE, 11 * TILE_SIZE, "KEY_08", true);
+
+        return new InteractableObject[]{key, door, treasure,key2};
     }
 
     public void drawInteractiveObjects(Graphics2D graphics2D) {
