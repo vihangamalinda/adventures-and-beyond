50be3924c66815c634900b8fe103bbfbc4e5260c
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-09-26 11:02:33 -0800

    fix:decreasing player's solid area.

--- adventures.and.beyond/src/helper/Constant.java
+++ adventures.and.beyond/src/helper/Constant.java
@@ -28,6 +28,6 @@ public class Constant {
     public final static int PLAYER_SOLID_AREA_START_X = 8 * PLAYER_UP_SCALE;
     public final static int PLAYER_SOLID_AREA_START_Y = 16 * PLAYER_UP_SCALE;
     public final static int PLAYER_SOLID_AREA_WIDTH = 32 * PLAYER_UP_SCALE;
-    public final static int PLAYER_SOLID_AREA_HEIGHT = 32 * PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_HEIGHT = 28 * PLAYER_UP_SCALE;
 
 }
