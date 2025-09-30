3a770e213d4d26503529348a486d7b5c815de2e5
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-01 03:20:50 +0530

    feat: Registering a new font constant.

--- adventures.and.beyond/src/ui/RegisteredFonts.java
+++ adventures.and.beyond/src/ui/RegisteredFonts.java
@@ -6,4 +6,7 @@ public class RegisteredFonts {
     public static Font STANDARD_ARIAL = new Font("Arial",Font.PLAIN,40);
     public static Font PlAIN_ARIAL_XSM = new Font("Arial",Font.PLAIN,12);
 
+    public static Font BOLD_ARIAL_XXL = new Font("Arial",Font.BOLD,60);
+
+
 }
