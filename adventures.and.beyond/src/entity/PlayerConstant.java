400b0d1402096d26fb3bdd9f152aa9383a9445fc
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-02 05:06:01 +0530

    fix:fixing solid area.

--- adventures.and.beyond/src/entity/PlayerConstant.java
+++ adventures.and.beyond/src/entity/PlayerConstant.java
@@ -5,8 +5,8 @@ public class PlayerConstant {
     public static final int PLAYER_UP_SCALE = 1;
 
     //Player Solid Area
-    public final static int PLAYER_SOLID_AREA_START_X = 8 * PLAYER_UP_SCALE;
-    public final static int PLAYER_SOLID_AREA_START_Y = 16 * PLAYER_UP_SCALE;
-    public final static int PLAYER_SOLID_AREA_WIDTH = 32 * PLAYER_UP_SCALE;
-    public final static int PLAYER_SOLID_AREA_HEIGHT = 28 * PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_START_X = 20 * PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_START_Y = 25 * PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_WIDTH = 8 * PLAYER_UP_SCALE;
+    public final static int PLAYER_SOLID_AREA_HEIGHT = 14 * PLAYER_UP_SCALE;
 }
