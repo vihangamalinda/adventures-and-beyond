368aadfa6ee8fc4d3661cb7acd84f84cfd2f1468
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-13 15:05:49 +0530

    feat: Adding property to determine if the character is on collision with player.

--- adventures.and.beyond/src/entity/npc/NonPlayerCharacter.java
+++ adventures.and.beyond/src/entity/npc/NonPlayerCharacter.java
@@ -8,12 +8,32 @@ import java.awt.*;
 public class NonPlayerCharacter extends Entity {
 
     private final String characterAnimationKey;
+    private boolean isOnCollisionWithPlayer;
     public NonPlayerCharacter(int worldPositionX, int worldPositionY, int speed, Direction direction, boolean isIdle, Rectangle solidArea, boolean onCollision, String characterAnimationKey) {
         super(worldPositionX, worldPositionY, speed, direction, isIdle, solidArea, onCollision);
         this.characterAnimationKey = characterAnimationKey;
+        this.isOnCollisionWithPlayer =false;
     }
 
     public String getCharacterAnimationKey() {
         return this.characterAnimationKey;
     }
+
+    protected boolean isOnCollisionWithPlayer() {
+        return isOnCollisionWithPlayer;
+    }
+
+
+    protected void setOnCollisionWithPlayer(boolean onCollisionWithPlayer) {
+        isOnCollisionWithPlayer = onCollisionWithPlayer;
+    }
+
+
+    public void associateWithPlayer(){
+        this.setOnCollisionWithPlayer(true);
+    }
+    public void disassociateWithPlayer(){
+        this.setOnCollisionWithPlayer(false);
+    }
+
 }
