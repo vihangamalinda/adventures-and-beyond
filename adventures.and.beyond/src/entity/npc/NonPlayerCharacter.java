7571155be0e679d58f8de686ae860ddff078a44f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-13 15:07:12 +0530

    refactor: moving the  responsibility  to super class.

--- adventures.and.beyond/src/entity/npc/NonPlayerCharacter.java
+++ adventures.and.beyond/src/entity/npc/NonPlayerCharacter.java
@@ -36,4 +36,22 @@ public class NonPlayerCharacter extends Entity {
         this.setOnCollisionWithPlayer(false);
     }
 
+    public Rectangle getSolidAreaWithWorldPositions() {
+        Rectangle currentSolidArea = this.getSolidArea();
+        int x = 0;
+        int y = 0;
+        boolean isMoving = !this.isIdle();
+
+        if(isMoving) {
+            int speed = this.getSpeed();
+            switch (this.getDirection()) {
+                case FACING_BACKWARD -> y = -speed;
+                case FACING_FORWARD -> y = +speed;
+                case FACING_LEFTWARD -> x = -speed;
+                case FACING_RIGHTWARD -> x = +speed;
+                default -> System.out.println("Custom error");
+            }
+        }
+        return new Rectangle(this.getWorldPositionX() +x, this.getWorldPositionY() +y, currentSolidArea.width, currentSolidArea.height);
+    }
 }
