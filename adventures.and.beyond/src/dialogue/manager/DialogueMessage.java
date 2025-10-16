b707216c4933772c679c4846966b90232c30163e
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-16 21:34:11 +0530

    feat:Reference materials related to dialogue messages.

---
+++ adventures.and.beyond/src/dialogue/manager/DialogueMessage.java
@@ -0,0 +1,7 @@
+package dialogue.manager;
+
+public class DialogueMessage {
+    public static final String DIALOGUE_01_MESSAGE ="HI, I'm Vihanga 1";
+    public static final String DIALOGUE_02_MESSAGE ="HI, I'm Vihanga 2";
+
+}
