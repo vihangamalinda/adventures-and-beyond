41e9b6820aa3ab8773ed1860b310051d15416215
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-21 05:00:21 +0530

    feat:Invoking the defined InteractableObject builders .

--- adventures.and.beyond/src/object/InteractableObjectManager.java
+++ adventures.and.beyond/src/object/InteractableObjectManager.java
@@ -27,11 +27,14 @@ public class InteractableObjectManager {
     }
 
     private InteractableObject[] initializeInteractableObjects() {
-        InteractableObject key = new KeyObject(false, 28 * TILE_SIZE, 17 * TILE_SIZE, "KEY_07", true);
-        InteractableObject door = new DoorObject(false, 22 * TILE_SIZE, 25 * TILE_SIZE, "DOOR_5", "KEY_07", true);
 
-        InteractableObject treasure = new TreasureObject(false, 22 * TILE_SIZE, 27 * TILE_SIZE, "TREASURE_05", "KEY_08", true);
-        InteractableObject key2 = new KeyObject(false, 16 * TILE_SIZE, 11 * TILE_SIZE, "KEY_08", true);
+        String keyCode01 = "KEY_07";
+        InteractableObject key = KeyObject.getBuilder().onCollision(false).worldPositionX(28 * TILE_SIZE).worldPositionY(17 * TILE_SIZE).keyCode(keyCode01).isActive(true).build();
+        InteractableObject door = DoorObject.getBuilder().onCollision(false).worldPositionX(22 * TILE_SIZE).worldPositionY(25 * TILE_SIZE).doorNumber("DOOR_5").openCode(keyCode01).isActive(true).build();
+
+        String keyCode02 = "KEY_08";
+        InteractableObject treasure =TreasureObject.getBuilder().onCollision(false).worldPositionX(22 * TILE_SIZE).worldPositionY(27 * TILE_SIZE).treasureNumber("TREASURE_05").openCode(keyCode02).isActive( true).build();
+        InteractableObject key2 =KeyObject.getBuilder().onCollision(false).worldPositionX(16 * TILE_SIZE).worldPositionY(11 * TILE_SIZE).keyCode(keyCode02).isActive(true).build();
 
         return new InteractableObject[]{key, door, treasure, key2};
     }
