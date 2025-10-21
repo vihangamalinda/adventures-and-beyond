4759b388d8ca1d17b41ac608352761078b3fa9f4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-21 21:10:17 +0530

    feat: exposing worldmap related interactable objects array.

--- adventures.and.beyond/src/world/WorldMapInformation.java
+++ adventures.and.beyond/src/world/WorldMapInformation.java
@@ -1,6 +1,9 @@
 package world;
 
+import object.interactable.objects.InteractableObject;
+
 public interface WorldMapInformation {
     int getTileKey(int rowIndex, int colIndex);
     boolean canTileBeCollided(int rowIndex, int colIndex);
+    InteractableObject[] getInteractableObjects();
 }
