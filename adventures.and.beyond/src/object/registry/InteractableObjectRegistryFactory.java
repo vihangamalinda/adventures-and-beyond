02bba735649e2a6bf5e256868dc38f99811d9118
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-10-21 21:03:08 +0530

    feat:Exposing a single instance of Interactable registry through the factory.

---
+++ adventures.and.beyond/src/object/registry/InteractableObjectRegistryFactory.java
@@ -0,0 +1,10 @@
+package object.registry;
+
+public class InteractableObjectRegistryFactory {
+    static  class Holder{
+        private static final InteractableObjectRegistry INSTANCE= new InteractableObjectRegistryImpl();
+    }
+    public static InteractableObjectRegistry getInstance(){
+        return Holder.INSTANCE;
+    }
+}
