3c2ea0457fc211afbaa9b5e327d9c08a1ed4712d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-25 13:49:50 +0530

    refactor: expsoing a state object which represent users inputs related to player.

---
+++ adventures.and.beyond/src/keyhandler/playerinputstate/registeredplayerinput/RegisteredPlayerInput.java
@@ -0,0 +1,18 @@
+package keyhandler.playerinputstate.registeredplayerinput;
+
+public class RegisteredPlayerInput {
+    public final boolean upPressed;
+    public final boolean downPressed;
+    public final boolean leftPressed;
+    public final boolean rightPressed;
+    public final boolean isAtIdle;
+
+    public RegisteredPlayerInput(boolean upPressed, boolean downPressed, boolean leftPressed, boolean rightPressed,boolean isAtIdle) {
+        this.upPressed = upPressed;
+        this.downPressed = downPressed;
+        this.leftPressed = leftPressed;
+        this.rightPressed = rightPressed;
+        this.isAtIdle = isAtIdle;
+    }
+
+}
