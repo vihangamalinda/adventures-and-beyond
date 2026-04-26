8107a934e80f0d84f48a9547de4d33f58fd8617b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-26 18:59:56 +0530

    feat: integrating a configuration class which is responsible on defining the instances of this application.

--- adventures.and.beyond/src/main/Main.java
+++ adventures.and.beyond/src/main/Main.java
@@ -1,26 +1,9 @@
 package main;
 
-import entity.manager.EntityManager;
-import entity.manager.EntityManagerFactory;
+import configuration.AppConfiguration;
 import entity.player.PlayerSpriteManager;
-import game.controller.GameController;
-import game.controller.GameControllerImpl;
-import game.loop.GameLoop;
-import game.loop.GameLoopImpl;
-import game.panel.GamePanel;
-import game.render.GameRenderer;
-import game.render.GameRendererImpl;
-import game.update.GameUpdater;
-import game.update.GameUpdaterImpl;
-import input.KeyHandler;
-import input.reader.UIInputReader;
-import input.reader.UIInputReaderImpl;
-import input.state.ReadKeyState;
-import ui.UserInterfaceManager;
 import window.GameWindow;
 import window.GameWindowImpl;
-import world.manager.WorldMapManager;
-import world.manager.WorldMapManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,32 +11,11 @@ public class Main {
 
         String title = "Adventures and Beyond";
 
-        GameController gameController = createGameController();
+        AppConfiguration appConfiguration = new AppConfiguration();
+
         GameWindow gameWindow = new GameWindowImpl(title);
-        gameWindow.initialize(gameController.getGamePanel());
-        gameController.startGame();
-    }
-
-    private static GameController createGameController() {
-        EntityManager entityManager = EntityManagerFactory.getInstance();
-        UserInterfaceManager userInterfaceManager = UserInterfaceManager.getInstance();
-        WorldMapManager worldMapManager = WorldMapManagerFactory.getInstance();
-        KeyHandler keyHandler = KeyHandler.getInstance();
-        ReadKeyState readKeyState = keyHandler.readRegisteredKeyState();
-        UIInputReader uiInputReader = new UIInputReaderImpl(readKeyState);
-
-        GameUpdater gameUpdater = new GameUpdaterImpl(entityManager,
-                                                      userInterfaceManager,
-                                                      uiInputReader);
-        GameLoop gameLoop = new GameLoopImpl(gameUpdater);
-        GameRenderer gameRenderer = new GameRendererImpl(entityManager,
-                                                         userInterfaceManager,
-                                                         worldMapManager);
-        GamePanel gamePanel = new GamePanel(gameRenderer,
-                                            keyHandler);
-
-        return new GameControllerImpl(gameLoop,
-                                      gamePanel);
+        gameWindow.initialize(appConfiguration.getGamePanel());
+        appConfiguration.getGameController().startGame();
     }
 }
 
