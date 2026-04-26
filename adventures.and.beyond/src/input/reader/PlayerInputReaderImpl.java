52c1dd52705ae419e2bd429416c13aaa2f87a7c3
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-26 18:57:23 +0530

    comment: Commenting code that might be need in future if this logic needs to be optimized.

--- adventures.and.beyond/src/input/reader/PlayerInputReaderImpl.java
+++ adventures.and.beyond/src/input/reader/PlayerInputReaderImpl.java
@@ -5,10 +5,26 @@ import input.state.ReadKeyState;
 
 public class PlayerInputReaderImpl implements PlayerInputReader {
     private final ReadKeyState readKeyState;
+//    private final RegisteredPlayerInput registeredPlayerInput;
 
     public PlayerInputReaderImpl(ReadKeyState readKeyState) {
         this.readKeyState = readKeyState;
+//        this.registeredPlayerInput = new RegisteredPlayerInput(
+//                this.readKeyState.isUpPressed(),
+//                this.readKeyState.isDownPressed(),
+//                this.readKeyState.isLeftPressed(),
+//                this.readKeyState.isRightPressed(),
+//                !(this.readKeyState.isAnyMovementDirectionKeyPressed())
+//        );
     }
+
+//    private void updateRegisteredPlayerInput() {
+//        this.registeredPlayerInput.upPressed = this.readKeyState.isUpPressed();
+//        this.registeredPlayerInput.downPressed = this.readKeyState.isDownPressed();
+//        this.registeredPlayerInput.leftPressed = this.readKeyState.isLeftPressed();
+//        this.registeredPlayerInput.rightPressed = this.readKeyState.isRightPressed();
+//        this.registeredPlayerInput.isAtIdle = !(this.readKeyState.isAnyMovementDirectionKeyPressed());
+//    }
 
     @Override
     public RegisteredPlayerInput readPlayerInput() {
@@ -20,5 +36,10 @@ public class PlayerInputReaderImpl implements PlayerInputReader {
                 this.readKeyState.isRightPressed(),
                 isAtIdle
         );
+//        this.updateRegisteredPlayerInput();
+//        if(this.registeredPlayerInput.downPressed){
+//            System.out.println("down is pressed");
+//        }
+//        return this.registeredPlayerInput;
     }
 }
