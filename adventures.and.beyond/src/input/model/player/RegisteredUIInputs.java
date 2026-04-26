7982476778dd60553be760afb6293e132c5bcff2
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-26 09:58:37 +0530

    refactor: New model to represent UI inputs.

---
+++ adventures.and.beyond/src/input/model/player/RegisteredUIInputs.java
@@ -0,0 +1,9 @@
+package input.model.player;
+
+public class RegisteredUIInputs {
+    public final boolean pausePressed;
+
+    public RegisteredUIInputs(boolean pausePressed) {
+        this.pausePressed = pausePressed;
+    }
+}
